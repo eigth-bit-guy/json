f03e7cdd3343dd3e25e3734e7e100c399b5d459e
Sam <157816793+eigth-bit-guy@users.noreply.github.com>
2024-02-12 15:42:17 -0300

    add license in source code

--- main.c
+++ main.c
@@ -1,3 +1,20 @@
+/* This program aims to separate the values ​​of a json */
+
+/* Copyright (C) 2024  Samuel Henrique */
+
+/* This program is free software: you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or */
+/* (at your option) any later version. */
+
+/* This program is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+/* GNU General Public License for more details. */
+
+/* You should have received a copy of the GNU General Public License */
+/* along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -133,13 +150,13 @@ int main(int argc, char *argv[])
   int i, len;
 
   if(argc < 2){
-	printf("Nenhum par�metro foi passado\n");
+	printf("Nenhum parâmetro foi passado\n");
 	return 1;
   }
   
   len = readStream(stream, argv[1]);
   if(len == -1) printf("Erro ao ler o stream\n");
-  else if(len > MAX_CHARS) printf("Arquivo muito grande, char m�x: %i\n\n", MAX_CHARS);
+  else if(len > MAX_CHARS) printf("Arquivo muito grande, char máx: %i\n\n", MAX_CHARS);
   else printf("Arquivo lido com sucesso, quantidade de caracters do json: %d\n\n", len);
   
   i = tokenize(ptrStream, ptrTokens, len);
