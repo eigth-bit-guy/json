49a8ac31b2361fd0b3f3f708a6a01766b40db57f
Sam <157816793+eigth-bit-guy@users.noreply.github.com>
2024-06-30 16:21:17 -0300

    Update libjson.c
    remove redundancy methods

--- src/libjson.c
+++ src/libjson.c
@@ -17,50 +17,6 @@ data_json new_json()
   return (data_json*)malloc(sizeof(data_json));
 }
 
-int analyse_json_object(char **json, tree_data_json *object)
-{
-  int result;
-
-  result = map_json_object(json, object);
-  if(result != 0){
-	fprintf(stderr, "Error: when map json: %s\n", strerror(errno));
-	return 1;
-  }else
-	printf("Map success\n");
-
-  result = parser_simple_json(json, object);
-  if(result  != 0 ){
-	fprintf(stderr, "Error: when parser json: %s\n", strerror(errno));
-	return 1;
-  }else
-	printf("Parse success\n");
-
-  return 0;
-}
-
-int map_json_object(char **json, tree_data_json *object)
-{
-  char *start_key,
-	*end_key;
-  int result,
-	counter_caracters = 0,
-	start,
-	end;
-  
-  size_t lenght;
-  lenght = strlen(*json);
-  object->json_lenght = lenght;
-
-  start_key = strchr(*json, '{');
-  object->start = (int)(start_key - *json + 1);
-  
-  end_key =  strchr(*json, '}');
-  object->end = (int)(end_key - *json + 1);
-  
-  return 0;
-}
-
-
 int parser_simple_json(char **json, tree_data_json *object)
 {
   for(size_t i = 0; i < object->json_lenght; i++) {
